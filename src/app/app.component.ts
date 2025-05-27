2faad0813e02457331dd9120f8866b78ed68c071
Elioce33 <eliott.collin0307@gmail.com>
2025-05-27 11:16:33 +0200

    refactor: create html file for app template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,44 +7,7 @@ import { classesActions } from '../store/classes/classes.actions';
 @Component({
   selector: 'app-root',
   imports: [RouterModule, RouterLink, RouterLinkActive],
-  template: `
-    <header>
-      <h1>D&D information here</h1>
-      <nav>
-        <ul>
-          <li>
-            <a class="ribbon" routerLink="/" routerLinkActive="active"
-               [routerLinkActiveOptions]="{ exact: true }" ariaCurrentWhenActive="page">Home</a>
-          </li>
-          <li>
-            <a class="ribbon" routerLink="/classes" routerLinkActive="active"
-               [routerLinkActiveOptions]="{ exact: true }" ariaCurrentWhenActive="page">Classes</a>
-          </li>
-          <li>
-            <a class="ribbon" routerLink="/spells" routerLinkActive="active"
-               [routerLinkActiveOptions]="{ exact: true }" ariaCurrentWhenActive="page">Spells</a>
-          </li>
-          <li>
-            <a class="ribbon" routerLink="/equipments" routerLinkActive="active"
-               [routerLinkActiveOptions]="{ exact: true }" ariaCurrentWhenActive="page">Equipment</a>
-          </li>
-          <li>
-            <a class="ribbon" routerLink="/about" routerLinkActive="active"
-               [routerLinkActiveOptions]="{ exact: true }" ariaCurrentWhenActive="page">About</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-    <!-- The routed views render in the <router-outlet>-->
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-    <footer>
-      <div>
-        Copyright 2025 Eliott COLLIN
-      </div>
-    </footer>
-    `,
+  templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
